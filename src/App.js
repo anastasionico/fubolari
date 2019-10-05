c2bd628b2b6a49bf6fca398bb8d2b6fad2fd2f4c
anastasionico <anastasionico@gmail.com>
2019-10-05 19:37:55 +0100

    layout footer mobile done

--- src/App.js
+++ src/App.js
@@ -19,11 +19,7 @@ function App() {
       <Courses></Courses>
       <ContactUs></ContactUs>
       <Sponsor></Sponsor>
-      {/*
-            
-            
-            <Footer></Footer>
-            */}
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,36 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<div>
-			<h1>This is Footer Section</h1>	
+		<div id='footer'>
+			<div id='footer-left'>
+				<img src='https://images.pexels.com/photos/2065820/pexels-photo-2065820.jpeg'/>
+				<h3>About Us</h3>
+				<ul>
+					<li>How are why</li>
+					<li>What we do</li>
+					<li>Privacy</li>
+					<li>Terms & Condition</li>
+				</ul>
+			</div>
+			<div id='footer-right'>
+				<h3>Contact Us</h3>
+				
+				<ul className='footer-social'>
+					<li><i class="lni-facebook-filled"></i></li>
+					<li><i class="lni-instagram-filled"></i></li>
+					<li><i class="lni-linkedin-filled"></i></li>
+				</ul>
+				
+				<ul>
+					<li>Where we are</li>
+					<li>Our Contact</li>
+					<li>News</li>
+				</ul>
+				<ul>
+					<li>ouremail@email.com</li>
+					<li>987 654321</li>
+				</ul>
+			</div>
 		</div>
 	)
 }
