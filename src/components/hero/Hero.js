f12ea4a93bb1d6fbff4b9254203ca9bd1d5d97cb
anastasionico <anastasionico@gmail.com>
2019-10-07 23:29:11 +0100

    update Nav to class

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,24 +3,10 @@ import Nav from './Nav'
 import HeroContent from './HeroContent'
 
 class Hero extends Component {
-
-	constructor() {
-		super()
-		this.state = { username:"Login" }
-		
-	}
-
-	changeUsername() {
-		this.setState({
-			username:"Nico"
-		})	
-	}
-	
-
 	render () {	
 		return (
 			<div id="hero">
-				<Nav username={this.state.username}/>
+				<Nav/>
 				<HeroContent/>
 			</div>	
 		)
--- src/components/hero/Nav.js
+++ src/components/hero/Nav.js
@@ -1,43 +1,65 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Nav = (props) => {
-	return (
-		<div className='nav'>
-			<nav className='nav-pages'>
-				<div className='nav-pages-logo'>
-					<img src='https://images.pexels.com/photos/2065820/pexels-photo-2065820.jpeg'/>	
-				</div>
-				<div className='nav-pages-list'>
+class Nav extends Component {
+	constructor () {
+		super()
+		this.state = {
+			username : 'login'
+		}
+	}
+
+	changeUsername() {
+		if (this.state.username == 'login') {
+			this.setState({
+				username : 'Nico'	
+			})	
+		} else {
+			this.setState({
+				username : 'login'	
+			})	
+		}
+		
+	}
+
+	render () {
+		return (
+			<div className='nav'>
+				<nav className='nav-pages'>
+					<div className='nav-pages-logo'>
+						<img src='https://images.pexels.com/photos/2065820/pexels-photo-2065820.jpeg'/>	
+					</div>
+					<div className='nav-pages-list'>
+						<ul>
+							<li>News</li>
+							<li>Subscribe</li>
+							<li>Courses</li>
+							<li>Contact</li>
+						</ul>
+					</div>
+					<div className='clearfix'></div>
+				</nav>
+				
+				<nav className='nav-auth'>
 					<ul>
-						<li>News</li>
-						<li>Subscribe</li>
-						<li>Courses</li>
-						<li>Contact</li>
+						<li>
+							<button className='btn btn-primary' onClick = {() => this.changeUsername()}>
+								{this.state.username}
+							</button>
+						</li>
+						<li>Register</li>
 					</ul>
-				</div>
+				</nav>
+				
+				<form className='form-seach'>
+					<div className='form-seach-icon circle'>
+						<i className="lni-search size-sm"></i>
+					</div>
+					<input className='form-seach-text' type="text" name='search'/>
+				</form>
 				<div className='clearfix'></div>
-			</nav>
-			
-			<nav className='nav-auth'>
-				<ul>
-					<li>
-					<	button className='btn btn-primary' onClick = {() => this.changeUsername()}>
-							{props.username}
-						</button>
-					</li>
-					<li>Register</li>
-				</ul>
-			</nav>
-			
-			<form className='form-seach'>
-				<div className='form-seach-icon circle'>
-					<i className="lni-search size-sm"></i>
-				</div>
-				<input className='form-seach-text' type="text" name='search'/>
-			</form>
-			<div className='clearfix'></div>
-		</div>	
-	)
+			</div>	
+		)
+	}
 }
 
 export default Nav
