1c6b69c002fa7226c257c17c7549fba43d0f9a74
anastasionico <anastasionico@gmail.com>
2019-11-05 23:35:17 +0000

    image added to sponsor

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -1,13 +1,31 @@
 import React from 'react'
 
+const sponsors = [
+	'https://www.wonkypeach.com/wp-content/uploads/2017/11/24169362_1413578255428529_1371695503_o.jpg',
+	'https://www.wonkypeach.com/wp-content/uploads/2017/11/24167221_1413578262095195_1941316237_o.jpg',
+	'https://www.wonkypeach.com/wp-content/uploads/2017/11/24167357_1413578258761862_1515427379_o.jpg',
+	'https://i.pinimg.com/originals/42/25/13/4225134c321df29f7a9b0c82be5bb79f.jpg'
+]
+
+
+
 const Sponsor = () => {
+	let image = React.createRef();
+
+	// the following function is attribuited always to the last image fix this error
+	const enlarge = () => {
+		console.log(image.current.src)
+		image.current.classList.add('clicked');
+	}
+
 	return (
 		<div id='sponsor'>
 			<h2>Our Sponsor</h2>
-			<img className='sponsor-images' alt='sponsor' src='https://images.pexels.com/photos/2065820/pexels-photo-2065820.jpeg'/>
-			<img className='sponsor-images' alt='sponsor' src='https://images.pexels.com/photos/2065820/pexels-photo-2065820.jpeg'/>
-			<img className='sponsor-images' alt='sponsor' src='https://images.pexels.com/photos/2065820/pexels-photo-2065820.jpeg'/>
-			<img className='sponsor-images' alt='sponsor' src='https://images.pexels.com/photos/2065820/pexels-photo-2065820.jpeg'/>
+			{sponsors.map(sponsor => {
+				return <img className='sponsor-images' alt='sponsor' src={sponsor} ref={image} onClick={enlarge}/>	
+			})}
+			
+			
 		</div>
 	)
 }
