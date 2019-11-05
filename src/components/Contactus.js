93aba0b5ad25bea1055c56ccc1d3b786ecd9dfbe
anastasionico <anastasionico@gmail.com>
2019-11-05 23:13:27 +0000

    add animation to subscribe form

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Contactus = () => {
-	return (
-		<div id='contactus'>
-			<div className='contact-panel'>
-				<form>
-					<input type='email' placeholder='Insert your email'/>
-					<input type='subject' placeholder='Insert the subject'/>
-					<textarea defaultValue="Insert your message" />
+class Contactus extends Component {
+	constructor () {
+		super()
+		this.responseDiv = React.createRef()
+		this.contactPanel = React.createRef()
+		this.responseMessage = this.responseMessage.bind(this);
+	}
 
-					<div className='contact-panel-link'>
-						<button type="submit">
-							<i className="lni-arrow-right-circle"></i>
-						</button>	
+	componentDidMount = () => {
+	}
+
+	responseMessage = (e) => {
+		e.preventDefault()
+		
+		const contactPanel = this.contactPanel.current;
+		contactPanel.classList.add('hidden')
+
+
+
+		const responseDiv = this.responseDiv.current;
+		responseDiv.classList.add('active')
+		
+
+		
+	}
+
+	render (){
+		return (
+			<React.Fragment>
+				<div id='contactus'>
+					<div className='contact-panel' ref={this.contactPanel}>
+						<form onSubmit={this.responseMessage}>
+							<input type='email' placeholder='Insert your email' value='a@a.com'/>
+							<input type='subject' placeholder='Insert the subject' value='a@a.com'/>
+							<textarea defaultValue="Insert your message"/>
+
+							<div className='contact-panel-link'>
+								<button type="submit">
+									<i className="lni-arrow-right-circle"></i>
+								</button>	
+							</div>
+						</form>
 					</div>
-				</form>
-			</div>
-		</div>
-	)
+					<div id='responseDiv' ref={this.responseDiv}>
+						<h1>this is a successfull response message</h1>
+					</div>
+				</div>
+				
+			</React.Fragment>
+		)
+		
+	}
 }
 
-export default Contactus
+
+export default Contactus 
